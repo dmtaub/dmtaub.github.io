acbdfb4f659dccf1d684f116998b3a2ef8cb1477
Daniel Taub <dmtaub@gmail.com>
2026-03-26 21:37:54 -0400

    package

--- apps/melody-spark/vite.config.ts
+++ apps/melody-spark/vite.config.ts
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/melody-spark/',
+  build: {
+    outDir: '../../melody-spark',
+    emptyOutDir: true,
+  },
 });
